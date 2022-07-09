57420428237d8cabe4c5e8d3d087211e08c7941b
Ryeokyoung <ryeokyoungk@gmail.com>
2022-07-09 22:16:17 +0900

    -

---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,15 @@
+package com.javaex.practice;
+
+public class Ex02 {
+
+	public static void main (String[] agrgs) {
+		
+	int ture;
+	int _of_workers;
+	int countOfLettersInString;
+	int stLevel1;
+	int person$;
+	int _person;
+	
+	}
+}
